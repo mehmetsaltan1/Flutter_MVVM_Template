0bd60aafbc9de60161f8111ca233d82ef447eeb4
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2021-12-02 17:12:28 +0300

    added mobx to project

--- lib/view/test/view/test_view.dart
+++ lib/view/test/view/test_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_mvvm_template/core/base/state/base_state.dart';
 import 'package:flutter_mvvm_template/core/base/widget/base_widget.dart';
 import 'package:flutter_mvvm_template/view/test/viewmodel/test_view_model.dart';
@@ -11,10 +12,19 @@ class Testview extends StatefulWidget {
 }
 
 class _TestviewState extends BaseState<Testview> {
+  TestViewModel viewModel;
+
   @override
   Widget build(BuildContext context) {
     return BaseView<TestViewModel>(
-        viewmodel: TestViewModel(), onPageBuilder: (context, value) => Text("data")); //Base widgettan türettiğimiz sayfa
+        viewmodel: TestViewModel(),
+        onModelReady: (model) {
+          viewModel = model;
+        },
+        onPageBuilder: (context, value) =>
+           scaffoldBody
+           ); //Base widgettan türettiğimiz sayfa
+
     // return Container(
     //   height: dynamicHeight(0.1), // Testview widget'ımız BaseState classından extend olduğu için bütün özelliklerini kullanabiliyor
     //                               // Bunu yapmamızın nedeni uygulamada değişiklik yapacağımız bir global değişkenin değerini her
@@ -22,4 +32,13 @@ class _TestviewState extends BaseState<Testview> {
     //   color: themeData.primaryColor,
     // );
   }
+
+  Widget get scaffoldBody => Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => viewModel.incrementCounter(),
+        ),
+        body: Observer(builder: (_) {
+          return Text(viewModel.number.toString());
+        }),
+      );
 }
--- lib/view/test/viewmodel/test_view_model.dart
+++ lib/view/test/viewmodel/test_view_model.dart
@@ -1,8 +1,14 @@
 import 'package:mobx/mobx.dart';
+
 part 'test_view_model.g.dart';
 
 class TestViewModel = _TestViewModelBase with _$TestViewModel;
 
 abstract class _TestViewModelBase with Store {
-  
+  @observable
+  int number = 0;
+  @action
+  void incrementCounter() {
+    number++;
+  }
 }
