fce747d5d1eceaaf576fa502f88bcbfbef7ced98
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2021-12-09 18:54:48 +0300

    shared preferences manager & enums & services added project

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mvvm_template/core/constants/app/app_constants.dart';
+import 'package:flutter_mvvm_template/core/init/cache/locale_manager.dart';
 import 'package:flutter_mvvm_template/core/init/navigation/navigation_route.dart';
 import 'package:flutter_mvvm_template/core/init/navigation/navigation_service.dart';
-import 'package:flutter_mvvm_template/generated/locale_keys.g.dart';
 import 'package:flutter_mvvm_template/view/test/view/test_view.dart';
 
 import 'core/init/lang/language_manager.dart';
 
-void main() => runApp(EasyLocalization(
-    child: MyApp(),
-    supportedLocales: LanguageManager.instance.supportedLocales,
-    path: ApplicationConstants.LANG_ASSET_PATH));
+void main() {
+  LocaleManager.preferencesinit();
+  runApp(EasyLocalization(
+      child: MyApp(),
+      supportedLocales: LanguageManager.instance.supportedLocales,
+      path: ApplicationConstants.LANG_ASSET_PATH));
+}
 
 class MyApp extends StatelessWidget {
   @override
